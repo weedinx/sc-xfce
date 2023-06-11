284bc251c01dfcc4c339fc56a88feb2d36576b29
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-10 21:42:29 -0300

    Update sc-xfce.sh

--- sc-xfce.sh
+++ sc-xfce.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-apt update && apt upgrade -s
+apt update && apt upgrade -y
 
 #-------------------------
 #Drivers
 #-------------------------
 
-apt install firmware-linux-nonfree firmware-iwlwifi firmware-atheros firmware-realtek -s
+apt install firmware-linux-nonfree firmware-iwlwifi firmware-atheros firmware-realtek -y
 
 #-------------------------
 #Xorg minimo
 #-------------------------
 
-apt --no-install-recommends install -s xserver-xorg-core
+apt --no-install-recommends install -y xserver-xorg-core
 #apt --no-install-recommends install -y xserver-xorg-video-vesa
 #apt --no-install-recommends install -y xserver-xorg-video-intel
 #apt --no-install-recommends install -y xserver-xorg-video-nouveau
 #apt --no-install-recommends install -y xserver-xorg-video-nvidia
 #apt --no-install-recommends install -y xserver-xorg-video-ati
-apt --no-install-recommends install -s xserver-xorg-video-fbdev
-apt --no-install-recommends install -s xserver-xorg
+apt --no-install-recommends install -y xserver-xorg-video-fbdev
+apt --no-install-recommends install -y xserver-xorg
 
 #-------------------------
 #Xfce minimo
 #-------------------------
 
-apt --no-install-recommends install -s xfwm4
-apt --no-install-recommends install -s xfce4-session
-apt --no-install-recommends install -s xfce4-panel
-apt --no-install-recommends install -s xfdesktop4
-apt --no-install-recommends install -s policykit-1
-apt --no-install-recommends install -s xfce4-power-manager
-apt --no-install-recommends install -s gtk2-engines gtk2-engines-pixbuf
-apt --no-install-recommends install -s x11-xserver-utils
-apt --no-install-recommends install -s libxfce4ui-utils
-apt --no-install-recommends install -s xfce4-appfinder
-apt --no-install-recommends install -s xfce4-settings
-apt --no-install-recommends install -s xfconf
-apt --no-install-recommends install -s xfce4-screenshooter
+apt --no-install-recommends install -y xfwm4
+apt --no-install-recommends install -y xfce4-session
+apt --no-install-recommends install -y xfce4-panel
+apt --no-install-recommends install -y xfdesktop4
+apt --no-install-recommends install -y policykit-1
+apt --no-install-recommends install -y xfce4-power-manager
+apt --no-install-recommends install -y gtk2-engines gtk2-engines-pixbuf
+apt --no-install-recommends install -y x11-xserver-utils
+apt --no-install-recommends install -y libxfce4ui-utils
+apt --no-install-recommends install -y xfce4-appfinder
+apt --no-install-recommends install -y xfce4-settings
+apt --no-install-recommends install -y xfconf
+apt --no-install-recommends install -y xfce4-screenshooter
 
 #-------------------------
 #Recomendados
@@ -46,62 +46,62 @@ apt --no-install-recommends install -s xfce4-screenshooter
 
 #### Basicos ###
 
-apt --no-install-recommends install -s lightdm
+apt --no-install-recommends install -y lightdm
 #apt --no-install-recommends install -y slim
-apt --no-install-recommends install -s lxterminal
-apt --no-install-recommends install -s lxtask
-apt --no-install-recommends install -s mousepad
+apt --no-install-recommends install -y lxterminal
+apt --no-install-recommends install -y lxtask
+apt --no-install-recommends install -y mousepad
 #-------------------------
-apt --no-install-recommends install -s network-manager-gnome
+apt --no-install-recommends install -y network-manager-gnome
 sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 #-------------------------
-apt --no-install-recommends install -s thunar
+apt --no-install-recommends install -y thunar
 
 
 
 #### Codecs e Audio/Video ###
 
-apt install -s libav-tools gstreamer1.0-libav
+apt install -y libav-tools gstreamer1.0-libav
 #apt-get install -y ffmpeg
-apt install -s gstreamer1.0-plugins-good
-apt install -s gstreamer1.0-plugins-bad
-apt install -s gstreamer1.0-plugins-ugly
-apt install -s gstreamer1.0-nice
+apt install -y gstreamer1.0-plugins-good
+apt install -y gstreamer1.0-plugins-bad
+apt install -y gstreamer1.0-plugins-ugly
+apt install -y gstreamer1.0-nice
 #apt --no-install-recommends install -y vlc qt4-qtconfig
-apt --no-install-recommends install -s vlc
-apt --no-install-recommends -s install pulseaudio
-apt --no-install-recommends install -s xfce4-pulseaudio-plugin
+apt --no-install-recommends install -y vlc
+apt --no-install-recommends -y install pulseaudio
+apt --no-install-recommends install -y xfce4-pulseaudio-plugin
 #apt-get install -y alsa-utils
 
 
 #### Complementos ###
-apt install qt5ct qt5-qtconfig adwaita-qt -s
-apt install paper-icon-theme moka-icon-theme papirus-icon-theme -s
-apt install flatpak gnome-software-plugin-flatpak gnome-software -s
+apt install qt5ct qt5-qtconfig adwaita-qt -y
+apt install paper-icon-theme moka-icon-theme papirus-icon-theme -y
+apt install flatpak gnome-software-plugin-flatpak gnome-software -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-apt install -s xfce4-whiskermenu-plugin
-apt install -s viewnior
-apt --no-install-recommends install -s cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip -s
+apt install -y xfce4-whiskermenu-plugin
+apt install -y viewnior
+apt --no-install-recommends install -y cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip -y
 #apt-get install -y xcompmgr
-apt --no-install-recommends install -s xfburn
+apt --no-install-recommends install -y xfburn
 apt install -y firefox-esr firefox-esr-l10n-pt-br
-apt --no-install-recommends install -s atril
+apt --no-install-recommends install -y atril
 #apt-get --no-install-recommends install -y desktop-base
-apt install --no-install-recommends libreoffice-writer libreoffice-gtk3 -s
-apt install libreoffice-l10n-pt-br -s
-apt install htop -s
-apt -s install qemu-kvm libvirt-daemon  bridge-utils virtinst libvirt-daemon-system
-apt -s install virt-top libguestfs-tools libosinfo-bin  qemu-system virt-manager
+apt install --no-install-recommends libreoffice-writer libreoffice-gtk3 -y
+apt install libreoffice-l10n-pt-br -y
+apt install htop -y
+apt -y install qemu-kvm libvirt-daemon  bridge-utils virtinst libvirt-daemon-system
+apt -y install virt-top libguestfs-tools libosinfo-bin  qemu-system virt-manager
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome*.deb -s
+apt install ./google-chrome*.deb -y
 apt install -f
 wget https://az764295.vo.msecnd.net/stable/b3e4e68a0bc097f0ae7907b217c1119af9e03435/code_1.78.2-1683731010_amd64.deb
-apt install ./code*.deb  -s
+apt install ./code*.deb  -y
 chmod 777 /usr/share/themes/
 chmod 777 /usr/share/themes/*
 chmod 777 /usr/share/backgrounds/
 chmod 777 /usr/share/backgrounds/*
 
-apt update && apt upgrade -s
+apt update && apt upgrade -y
 
 reboot
