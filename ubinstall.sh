8ddd01fbde4b65709a49119432251815818902ad
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-13 17:32:32 -0300

    Update ubinstall.sh

--- ubinstall.sh
+++ ubinstall.sh
@@ -125,7 +125,8 @@ sudo apt install -y yaru-theme*
 
 ###Instalção da inferface gráfica de rede###
 sudo apt install -y network-manager
-sudo apt install -y network-manager-gnome
+#sudo apt --no-install-recommends install -y
+#sudo apt install -y network-manager-gnome
 sudo systemctl enable systemd-networkd.service
 systemctl start systemd-networkd.service
 systemctl restart systemd-networkd.service
