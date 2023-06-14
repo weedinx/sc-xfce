50b3f179e4206384299c699291338bef46a52777
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-13 23:43:05 -0300

    Update programasinstall.sh

--- programasinstall.sh
+++ programasinstall.sh
@@ -6,24 +6,24 @@ sudo apt update && apt upgrade -y
 #Instalação de programas
 #-------------------------
 
-#sudo apt install --no-install-recommends libreoffice-writer libreoffice-gtk3 -y
-#sudo apt install libreoffice-l10n-pt-br -y
+sudo apt install --no-install-recommends libreoffice-writer libreoffice-gtk3 -y
+sudo apt install libreoffice-l10n-pt-br -y
 
 ## Instalação da interface do bluetooth###
-#apt install -y bluez pulseaudio-module-bluetooth
+apt install -y bluez pulseaudio-module-bluetooth
 #apt install -y blueman
 
-#wget https://download.virtualbox.org/virtualbox/7.0.8/virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb
-#apt install -y ./virtualbox*.deb
-#adduser and vboxusers
+wget https://download.virtualbox.org/virtualbox/7.0.8/virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb
+apt install -y ./virtualbox*.deb
+adduser and vboxusers
 
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#apt install -y ./google-chrome*.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome*.deb
 
-#wget https://az764295.vo.msecnd.net/stable/b3e4e68a0bc097f0ae7907b217c1119af9e03435/code_1.78.2-1683731010_amd64.deb
-#apt install -y ./code*.deb
+wget https://az764295.vo.msecnd.net/stable/b3e4e68a0bc097f0ae7907b217c1119af9e03435/code_1.78.2-1683731010_amd64.deb
+apt install -y ./code*.deb
 
-#wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb
-#apt install -y ./balena-etcher*.deb
+wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb
+apt install -y ./balena-etcher*.deb
 
 sudo apt update && apt upgrade -y
