535d5bfd8e3cb883e32ed119d77189fe73167ba8
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-15 11:41:09 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -4,21 +4,26 @@ sudo apt update && apt upgrade -y
 sudo apt install -y chromium-browser
 sudo apt --no-install-recommends install network-manager-gnome -y
 sudo apt --no-install-recommends install -y xfce4-helpers xfce4-indicator-plugin xfce4-notifyd xfce4-panel xfce4-power-manager-data xfce4-power-manager-plugins xfce4-power-manager xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-session xfce4-settings xfce4-terminal xfce4-whiskermenu-plugin xfconf xfdesktop4-data xfdesktop4 xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xfwm4 xserver-xorg xterm xfce4-diskperf-plugin
-sudo apt --no-install-recommends install -y xiccd xinit xinput xkb-data xml-core xorg-docs-core xorg xserver-common xserver-xorg-core xserver-xorg-input-all xserver-xorg-input-libinput xserver-xorg-input-synaptics xserver-xorg-input-wacom xserver-xorg-legacy xserver-xorg-video-all xserver-xorg-video-amdgpu xserver-xorg-video-ati xserver-xorg-video-fbdev xserver-xorg-video-intel xserver-xorg-video-nouveau xserver-xorg-video-qxl xserver-xorg-video-radeon xserver-xorg-video-vesa xserver-xorg-video-vmware
-sudo apt --no-install-recommends install -y cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip -y
+sudo apt --no-install-recommends install -y keyboard-configuration xiccd xinit xinput xkb-data xml-core xorg-docs-core xorg xserver-common xserver-xorg-core xserver-xorg-input-all xserver-xorg-input-libinput xserver-xorg-input-synaptics xserver-xorg-input-wacom xserver-xorg-legacy xserver-xorg-video-all xserver-xorg-video-amdgpu xserver-xorg-video-ati xserver-xorg-video-fbdev xserver-xorg-video-intel xserver-xorg-video-nouveau xserver-xorg-video-qxl xserver-xorg-video-radeon xserver-xorg-video-vesa xserver-xorg-video-vmware
+sudo apt --no-install-recommends install -y software-properties-common software-properties-gtk sound-theme-freedesktop pulseaudio-utils pulseaudio cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip -y
+sudo apt --no-install-recommends install -yubuntu-advantage-desktop-daemon ubuntu-advantage-tools ubuntu-drivers-common ubuntu-keyring ubuntu-minimal ubuntu-release-upgrader-core ubuntu-standard
 sudo apt install network-manager -y
 sudo apt install linux-firmware -y
 sudo apt install -y thunar-data thunar-volman thunar xfce4-terminal thunar-media-tags-plugin thunar-archive-plugin
-sudo apt install -y curl zip unzip git htop xfce4-whiskermenu-plugin viewnior cpu-x gpick yaru-theme-gtk yaru-theme-icon
-apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman
+sudo apt install -y curl zip unzip git htop xfce4-whiskermenu-plugin viewnior cpu-x gpick yaru-theme-gtk yaru-theme-icon pavucontrol
+sudo apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman
+sudo apt install -y language-pack-gnome-pt-base language-pack-gnome-pt language-pack-pt-base language-pack-pt language-selector-commo
+sudo apt --no-install-recommends install -yubuntu-advantage-desktop-daemon ubuntu-advantage-tools ubuntu-drivers-common ubuntu-keyring ubuntu-minimal ubuntu-release-upgrader-core ubuntu-standard util-linux-extra util-linux
 
+
+
+software-properties-common software-properties-gtk sound-theme-freedesktop
 #######################
 sudo apt install --no-install-recommends libreoffice-writer libreoffice-gtk3 -y
 sudo apt install libreoffice-l10n-pt-br -y
 
 ## Instalação da interface do bluetooth###
-apt install -y bluez pulseaudio-module-bluetooth
-apt install -y blueman
+apt install -y synaptic
 
 wget https://download.virtualbox.org/virtualbox/7.0.8/virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb
 apt install -y ./virtualbox*.deb
@@ -33,9 +38,9 @@ apt install -y ./code*.deb
 wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb
 apt install -y ./balena-etcher*.deb
 
-chmod 777 /usr/share/themes/
-chmod 777 /usr/share/backgrounds/
-chmod 777 /usr/share/icons/
+chmod 777 /usr/share/themes/*
+chmod 777 /usr/share/backgrounds/*
+chmod 777 /usr/share/icons/*
 #######################
 
 sudo apt update && apt upgrade -y
